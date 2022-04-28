eccf1b208009d43448246dcd5d72eabb4a1727d8
Ayhal <vasilev2001as@gmail.com>
2022-04-29 06:19:48 +0900

    header css

--- main.go
+++ main.go
@@ -4,30 +4,43 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name       string
+	Age        uint16
+	Money      int16
+	Avg_grades float64
+	Hobbies    []string
 }
 
-func (u *User) getAllInfo() string {
-	return fmt.Sprintf("User name: %s\n Age: %d\nMoney: %d", u.name, u.age, u.money)
-}
+//type Subjects struct {
+// Subject string  `json:"subject"`
+// Score   float64 `json:"score"`
+//}
 
-func (u *User) setNewName(newName string) {
-	u.name = newName
+// func (u *User) getAllInfo() string {
+// 	return fmt.Sprintf("User name: %s\n Age: %d\nMoney: %d", u.Name, u.Age, u.Money)
+// }
+
+// func (u *User) setNewName(newName string) {
+// 	u.Name = newName
+// }
+
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	bob := User{"Bob", 25, -50, 4.2, 2.0}
+	bob := User{"Bob", 25, -50, 4.2, []string{"Info", "RED", "Coding"}}
 	//bob.money = -23
 	//bob.setNewName("asd")
 	//fmt.Fprintf(w, bob.getAllInfo())
-	tmpl, _ := template.ParseFiles("templates/home_page.html")
-	tmpl.Execute(w, bob)
+	tmpl.ExecuteTemplate(w, "home_page.html", bob)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
@@ -35,11 +48,43 @@ func contacts_page(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequest() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.HandleFunc("/", home_page)
 	http.HandleFunc("/contacts/", contacts_page)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":7272", nil)
 }
 
+// to start the server write in terminal > go run main.go
 func main() {
+	fmt.Println("Server is started (to stop press: 'ctrl' + 'c')")
 	handleRequest()
+	// db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/diary")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer db.Close()
+
+	// // insert, err := db.Query("INSERT INTO `subjects` (`subject`, `score`) VALUES('Математика', 5)")
+	// // if err != nil {
+	// // 	panic(err)
+	// // }
+	// // defer insert.Close()
+
+	// res, err := db.Query("SELECT `subject`, `score` FROM `subjects`")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for res.Next() {
+	// 	var subjects Subjects
+	// 	err = res.Scan(&subjects.Subject, &subjects.Score)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	// 	fmt.Println(fmt.Sprintf("Subject: %s, Score: %f", subjects.Subject, subjects.Score))
+	// }
+
 }
