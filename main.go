b2c1630cf7788d7d065a6a3b0247b0afbdd60827
Ayhal <vasilev2001as@gmail.com>
2022-05-01 20:42:54 +0900

    big upd

--- main.go
+++ main.go
@@ -13,16 +13,18 @@ import (
 type Subjects struct {
 	Id      uint16
 	Subject string
-	Sum     float32
+	Count   int16
 }
 
 type Todo_list struct {
 	Id         uint16
 	Subject_id uint16
 	Todo       string
-	Score      float32
+	Do         int16
 }
 
+var user = "root"
+var pswr = ""
 var subjs = []Subjects{}
 var todo_list = []Todo_list{}
 
@@ -31,28 +33,6 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/home_page.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		panic(err)
-	}
-
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/diary")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	res, err := db.Query("SELECT * FROM `subjects`")
-	if err != nil {
-		panic(err)
-	}
-
-	subjs = []Subjects{}
-	for res.Next() {
-		var subj Subjects
-		err = res.Scan(&subj.Id, &subj.Subject, &subj.Sum)
-		if err != nil {
-			panic(err)
-		}
-
-		subjs = append(subjs, subj)
 	}
 
 	t.ExecuteTemplate(w, "home_page", subjs)
@@ -64,40 +44,76 @@ func subjects_page(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	t.ExecuteTemplate(w, "subjects_page", nil)
-}
-
-/* ///////////////// ADD ///////////////// */
-func add_page(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/add_page.html", "templates/header.html", "templates/footer.html")
+	db, err := sql.Open("mysql", user+":"+pswr+"@tcp(127.0.0.1:3306)/diary")
 	if err != nil {
 		panic(err)
 	}
-	t.ExecuteTemplate(w, "add_page", nil)
+	defer db.Close()
+
+	res, err := db.Query("SELECT * FROM `subjects` ORDER BY `Id` DESC")
+	if err != nil {
+		panic(err)
+	}
+
+	subjs = []Subjects{}
+	for res.Next() {
+		var subj Subjects
+		err = res.Scan(&subj.Id, &subj.Subject)
+		if err != nil {
+			panic(err)
+		}
+
+		er := db.QueryRow("SELECT COUNT(*) FROM `todo_list` WHERE `do` = 0 AND `subject_id` = ?", subj.Id).Scan(&subj.Count)
+		if er != nil {
+			panic(er)
+		}
+
+		subjs = append(subjs, subj)
+	}
+
+	t.ExecuteTemplate(w, "subjects_page", subjs)
 }
 
 /* ///////////////// SAVE ///////////////// */
 func save(w http.ResponseWriter, r *http.Request) {
 	subj := r.FormValue("subj")
-	sum := r.FormValue("sum")
 
-	if subj == "" || sum == "" {
-		http.Redirect(w, r, "/add?error", http.StatusSeeOther)
+	if subj == "" {
+		http.Redirect(w, r, "/?error", http.StatusSeeOther)
 	} else {
-		db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/diary")
+		db, err := sql.Open("mysql", user+":"+pswr+"@tcp(127.0.0.1:3306)/diary")
 		if err != nil {
 			panic(err)
 		}
 		defer db.Close()
 
-		insert, err := db.Query(fmt.Sprintf("INSERT INTO `subjects` (`subject`, `sum`) VALUES('%s', '%s')", subj, sum))
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `subjects` (`subject`) VALUES('%s')", subj))
 		if err != nil {
 			panic(err)
 		}
 		defer insert.Close()
 
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+		http.Redirect(w, r, "/subjects/", http.StatusSeeOther)
 	}
+}
+
+/* ///////////////// DEL_SUBJ ///////////////// */
+func del_subj(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	db, err := sql.Open("mysql", user+":"+pswr+"@tcp(127.0.0.1:3306)/diary")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	upd, err := db.Query(fmt.Sprintf("DELETE FROM `subjects` WHERE `id`='%s'", vars["id"]))
+	if err != nil {
+		panic(err)
+	}
+	defer upd.Close()
+
+	http.Redirect(w, r, "/subjects/", http.StatusSeeOther)
 }
 
 /* ///////////////// SUBJECT ///////////////// */
@@ -109,28 +125,21 @@ func subject_show(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/diary")
+	db, err := sql.Open("mysql", user+":"+pswr+"@tcp(127.0.0.1:3306)/diary")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	res, err := db.Query(fmt.Sprintf("SELECT * FROM `todo_list` WHERE `subject_id` = '%s'", vars["id"]))
+	res, err := db.Query(fmt.Sprintf("SELECT * FROM `todo_list` WHERE `subject_id` = '%s' ORDER BY `Id` DESC", vars["id"]))
 	if err != nil {
 		panic(err)
 	}
 
-	// m := map[string]interface{}{}
-	// m["todo_list"] = []Todo_list{}
-	// m["title"], err = db.Query(fmt.Sprintf("SELECT `subject` FROM `subjects` WHERE `id` = '%s'", vars["id"]))
-	// if err != nil {
-	// 	panic(err)
-	// }
-
 	todo_list = []Todo_list{}
 	for res.Next() {
 		var todo Todo_list
-		err = res.Scan(&todo.Id, &todo.Subject_id, &todo.Todo, &todo.Score)
+		err = res.Scan(&todo.Id, &todo.Subject_id, &todo.Todo, &todo.Do)
 		if err != nil {
 			panic(err)
 		}
@@ -141,6 +150,74 @@ func subject_show(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "subject_show", todo_list)
 }
 
+/* ///////////////// SAVE_TASK ///////////////// */
+func save_task(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	task := r.FormValue("task")
+
+	if task == "" {
+		http.Redirect(w, r, "/?error", http.StatusSeeOther)
+	} else {
+		db, err := sql.Open("mysql", user+":"+pswr+"@tcp(127.0.0.1:3306)/diary")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `todo_list` (`subject_id`, `todo`, `do`) VALUES('%s', '%s', 0)", vars["id_subj"], task))
+		if err != nil {
+			panic(err)
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/subject/"+vars["id_subj"], http.StatusSeeOther)
+	}
+}
+
+/* ///////////////// DO ///////////////// */
+func do(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var do_make = 0
+
+	db, err := sql.Open("mysql", user+":"+pswr+"@tcp(127.0.0.1:3306)/diary")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	if vars["do"] == "1" {
+		do_make = 0
+	} else {
+		do_make = 1
+	}
+	upd, err := db.Query(fmt.Sprintf("UPDATE `todo_list` SET `do` = %d WHERE `id` = %s", do_make, vars["id_task"]))
+	if err != nil {
+		panic(err)
+	}
+	defer upd.Close()
+
+	http.Redirect(w, r, "/subject/"+vars["id_subj"], http.StatusSeeOther)
+}
+
+/* ///////////////// DEL ///////////////// */
+func del(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	db, err := sql.Open("mysql", user+":"+pswr+"@tcp(127.0.0.1:3306)/diary")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	upd, err := db.Query(fmt.Sprintf("DELETE FROM `todo_list` WHERE `id`='%s'", vars["id_task"]))
+	if err != nil {
+		panic(err)
+	}
+	defer upd.Close()
+
+	http.Redirect(w, r, "/subject/"+vars["id_subj"], http.StatusSeeOther)
+}
+
 /* ///////////////// handle ///////////////// */
 func handleRequest() {
 	rtr := mux.NewRouter()
@@ -148,12 +225,15 @@ func handleRequest() {
 	fs := http.FileServer(http.Dir("static"))
 	rtr.HandleFunc("/", home_page).Methods("GET")
 	rtr.HandleFunc("/subjects/", subjects_page).Methods("GET")
-	rtr.HandleFunc("/add/", add_page).Methods("GET")
 	rtr.HandleFunc("/save/", save).Methods("POST")
 	rtr.HandleFunc("/subject/{id:[0-9]+}", subject_show).Methods("GET")
+	rtr.HandleFunc("/del_subj/{id:[0-9]+}", del_subj).Methods("POST", "GET")
+	rtr.HandleFunc("/save_task/{id_subj:[0-9]+}", save_task).Methods("POST")
+	rtr.HandleFunc("/do/{id_subj:[0-9]+}/{id_task:[0-9]+}/{do:[0-1]}", do).Methods("POST", "GET")
+	rtr.HandleFunc("/del/{id_subj:[0-9]+}/{id_task:[0-9]+}", del).Methods("POST", "GET")
 
 	http.Handle("/", rtr)
-	http.Handle("/static/", http.StripPrefix("/static", fs))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.ListenAndServe(":7272", nil)
 }
 
